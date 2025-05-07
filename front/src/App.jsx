b099b0ee4bd6ec2891ec3acd4b7f7798670b42c7
x9t <imnxtgxxd@gmail.com>
2025-05-07 18:58:30 +0100

    i

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,27 +3,10 @@ import './App.css'
 import React, {use, useEffect, useState} from 'react'
 import axios from 'axios';
 function App() {
-  const [array, setArray] = useState([]);
-
-  const fetchAPI = async () =>{
-    const response = await axios.get("http://localhost:8080/api");
-    setArray(response.data.fruits)
-    console.log(response.data.fruits);
-  };
-
-  useEffect(() => {
-    fetchAPI()
-  }, [])
+  
   return (
     <>
-    {
-      array.map((fruits, index) =>(
-        <div key={index}>
-          <p>{fruits}</p>
-          <br></br>
-        </div>
-      ))
-    }
+    <center><h1>Hello haitham</h1></center>
     </>
   )
 }
