72bff7b8fb6d6bdea657e0e8837e6936839eb141
x9t <imnxtgxxd@gmail.com>
2025-05-07 18:15:19 +0100

    s

--- back/server.js
+++ back/server.js
@@ -1,4 +1,16 @@
-function sayHello(name){
-    console.log('hello'+name);
+const express = require('express')
+const app = express();
+const cors = require("cors");
+const corsOptions = {
+    origin: ["http://localhost:5173"],
 }
 
+app.use(cors(corsOptions));
+
+app.get("/api", (req, res)=>{
+    res.json({fruits :["apple","banaaaaana", "orange"]})
+})
+
+app.listen(8080,() => {
+    console.log("Server kissm3 f port 8080") 
+});
--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-
+import React, {use, useEffect, useState} from 'react'
+import axios from 'axios';
 function App() {
-  const [count, setCount] = useState(0)
+  const [array, setArray] = useState([]);
 
+  const fetchAPI = async () =>{
+    const response = await axios.get("http://localhost:8080/api");
+    setArray(response.data.fruits)
+    console.log(response.data.fruits);
+  };
+
+  useEffect(() => {
+    fetchAPI()
+  }, [])
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {
+      array.map((fruits, index) =>(
+        <div key={index}>
+          <p>{fruits}</p>
+          <br></br>
+        </div>
+      ))
+    }
     </>
   )
 }
