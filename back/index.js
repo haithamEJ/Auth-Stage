e7ba6390deafe53162dbfdb74a1ce905c6f5ff28
x9t <imnxtgxxd@gmail.com>
2025-05-08 09:57:12 +0100

    testing

--- back/index.js
+++ back/index.js
@@ -1,1 +1,10 @@
-console.log("hi")
+const speakeasy = require('speakeasy')
+const qrcode = require('qrcode')
+
+var secret = speakeasy.generateSecret({
+    name: "Haitham"
+})
+
+qrcode.toDataURL(secret.otpauth_url, function(err,data){
+    console.log(data)
+})
